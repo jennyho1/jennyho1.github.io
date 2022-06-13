ed903e0eda35166f8a960c4281480b46e561592d
Jenny Ho <jennhiho@gmail.com>
2022-06-13 02:05:53 -0400

    src folder

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
- # Jenny's website
+ # Jenny's Desk Setup Website
 
-Currently developing my personal website using vanilla JS, Three.js,...stuff
+Currently developing my personal website using Vite, vanilla JS, Three.js, ...
 
 ### Local setup
 1. `git clone https://github.com/jennyho1/jennyho1.github.io`
--- src/main.js
+++ src/main.js
@@ -1,21 +1,19 @@
 import './style.css'
 import * as THREE from 'three';
 
+
 // Setup
-
 const scene = new THREE.Scene();
-
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 });
 
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(20);
+camera.position.setZ(50);
 
-renderer.render(scene, camera);
+// renderer.render(scene, camera);
 
 const geometry = new THREE.TorusGeometry(10, 2, 16, 100);
 const material = new THREE.MeshBasicMaterial({ color: 0x6347FF, wireframe: true });
@@ -24,8 +22,8 @@ const torus = new THREE.Mesh(geometry, material);
 scene.add(torus);
 
 function animate() {
+  // 60 times per second
   requestAnimationFrame(animate);
-
   torus.rotation.x += 0.005;
   torus.rotation.y += 0.005;
   torus.rotation.z += 0.005;
