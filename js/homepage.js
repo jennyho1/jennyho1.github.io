f2527abfb0d574aa5e42990939c01f886970136f
Jenny Ho <jenny.ho@mail.utoronto.ca>
2024-06-22 12:00:45 -0400

    fix nav overlay

--- js/homepage.js
+++ js/homepage.js
@@ -17,7 +17,7 @@ gsap.to('.gear1', {
 		trigger: '.gear1',
 		scrub: 2
 	},
-	rotation: -400, 
+	rotation: 400, 
 	duration: 3,
 });
 gsap.to('.gear2', {
@@ -25,7 +25,7 @@ gsap.to('.gear2', {
 		trigger: '.gear2',
 		scrub: 2
 	},
-	rotation: 400, 
+	rotation: -400, 
 	duration: 3,
 });
 
@@ -50,35 +50,37 @@ gsap.to('.block-reveal.tv1', {
 });
 
 
-//  ----------- cursor following cat-----------
+document.addEventListener("DOMContentLoaded", function() {
 
+	//  ----------- cursor following cat-----------
+	// https://dev.to/anomaly3108/make-svg-follow-cursor-using-css-and-js-2okp
 
-let eyeball_left = document.querySelector(".eyeball_left"),
-			pupil_left = document.querySelector(".pupil_left"),
-		eyeArea_left = eyeball_left.getBoundingClientRect(),
+	let eyeball_left = document.querySelector(".eyeball_left"),
+	pupil_left = document.querySelector(".pupil_left"),
+	eyeArea_left = eyeball_left.getBoundingClientRect(),
 	pupil_leftArea = pupil_left.getBoundingClientRect(),
-					R_left = eyeArea_left.width/2,
-					r_left = pupil_leftArea.width/2,
-		centerX_left = eyeArea_left.left + R_left,
-		centerY_left = eyeArea_left.top + R_left;
+			R_left = eyeArea_left.width/2,
+			r_left = pupil_leftArea.width/2,
+	centerX_left = eyeArea_left.left + R_left,
+	centerY_left = eyeArea_left.top + R_left;
 
-let eyeball_right = document.querySelector(".eyeball_right"),
-			pupil_right = document.querySelector(".pupil_right"),
-		eyeArea_right = eyeball_right.getBoundingClientRect(),
+	let eyeball_right = document.querySelector(".eyeball_right"),
+	pupil_right = document.querySelector(".pupil_right"),
+	eyeArea_right = eyeball_right.getBoundingClientRect(),
 	pupil_rightArea = pupil_right.getBoundingClientRect(),
-					R_right = eyeArea_right.width/2,
-					r_right = pupil_rightArea.width/2,
-		centerX_right = eyeArea_right.left + R_right,
-		centerY_right = eyeArea_right.top + R_right;
+			R_right = eyeArea_right.width/2,
+			r_right = pupil_rightArea.width/2,
+	centerX_right = eyeArea_right.left + R_right,
+	centerY_right = eyeArea_right.top + R_right;
 
-document.addEventListener("mousemove", (e)=>{
+	document.addEventListener("mousemove", (e)=>{
 	let x_left = e.clientX - centerX_left,
-			y_left = e.clientY - centerY_left,
+	y_left = e.clientY - centerY_left,
 	theta_left = Math.atan2(y_left,x_left),
 	angle_left = theta_left*180/Math.PI + 360;
 
 	let x_right = e.clientX - centerX_right,
-			y_right = e.clientY - centerY_right,
+	y_right = e.clientY - centerY_right,
 	theta_right = Math.atan2(y_right,x_right),
 	angle_right = theta_right*180/Math.PI + 360;
 
@@ -89,4 +91,6 @@ document.addEventListener("mousemove", (e)=>{
 	pupil_right.style.transform = `translateX(${R_right - r_right +"px"}) rotate(${angle_right + "deg"})`;
 	pupil_right.style.transformOrigin = `${r_right +"px"} center`;
 
+	});
+  
 });
